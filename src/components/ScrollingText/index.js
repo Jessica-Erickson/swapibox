4fae5de0cbf052af2f9f25ac1c300a668bda00cb
Cole Hart <cole@colehart.com>
2018-10-09 16:53:05 -0600

    Add ScrollingText index.js and stateful class structure

---
+++ src/components/ScrollingText/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './ScrollingText.css'
+
+class ScrollingText extends Component {
+  render() {
+    return (
+      <aside className="ScrollingText">
+      </aside>
+    )
+  }
+}
+
+export default ScrollingText;
