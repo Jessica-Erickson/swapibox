a19ba4a832e18fd0b9a41c0b928249ee69560a59
Intrepid Panda <jerick999@gmail.com>
2018-10-08 19:54:31 -0600

    Set up as much css as expected is required

--- src/Card.js
+++ src/Card.js
@@ -16,11 +16,18 @@ export default class Card extends Component {
 
   render() {
     return (
-      <article className={'Card ' + (this.state.isActive ? 'isLit' : '')}>
-        <img 
-          src='lightsaber-wt.png' 
+      <article className={'Card ' + (this.state.isActive ? 'isAlsoLit' : '')}>
+        <h3>Luke Skywalker</h3>
+        <div className={'saber' + (this.state.isActive ? 'isLit' : '')} ></div>
+        <img
+          src='lightsaber-wt.png'
+          onMouseOver={this.src='lightsaber-bk.png'}
+          onMouseOut={this.src='lightsaber-wt.png'} 
           alt='The hilt of a lightsaber. Turn the lightsaber on to favorite this card.'
-          onClick={handleSaberClick} />
+          onClick={() => {
+            this.src='lightsaber-bk.png';
+            handleSaberClick();
+          }} />
       </article>
     )
   }
