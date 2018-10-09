183e18bff9b1f0201e4e72db94d6b551e2a7085b
Cole Hart <cole@colehart.com>
2018-10-09 17:55:48 -0600

    Add StarJedi ttf fonts in src/assets/fonts and restructure assets directory

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div className="App">
+        <ScrollingText />
+        <Header />
+        <CardContainer />
       </div>
     );
   }
