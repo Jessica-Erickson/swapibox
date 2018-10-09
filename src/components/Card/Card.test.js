6c4038a20639af32718e7b732f1d09c39021c4cd
Cole Hart <cole@colehart.com>
2018-10-09 17:05:22 -0600

    Add snapshot test to Card.test.js

---
+++ src/components/Card/Card.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Card from './index'
+
+describe('Card', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Card />)
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  }
+})
