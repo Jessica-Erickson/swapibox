568e69c4f249eae5b69afe373bcdf86ead76970e
Intrepid Panda <jerick999@gmail.com>
2018-10-10 16:14:13 -0600

    Write and pass test for getFilms api result if status ok

--- src/helper.js
+++ src/helper.js
@@ -2,4 +2,9 @@ export const getFilms = async () => {
   const url = 'https://swapi.co/api/films/';
   const response = await fetch(url);
 
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error('Status was not ok');
+  }
 }
--- src/helper.test.js
+++ src/helper.test.js
@@ -2,9 +2,24 @@ import * as API from './helper.js';
 
 describe('API', () => {
   describe('getFilms', () => {
-    beforeEach(() => {
-      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+    let mockFilms;
 
+    beforeEach(() => {
+      mockFilms = [
+        {title: 'Star Wars 1',
+          release_date: '1977-05-25',
+          opening_crawl: 'This is opening crawl 1'},
+        {title: 'Star Wars 2',
+          release_date: '1978-06-26',
+          opening_crawl: 'This is opening crawl 2'},
+        {title: 'Star Wars 3',
+          release_date: '1979-07-27',
+          opening_crawl: 'This is opening crawl 3'}
+        ];
+
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockFilms)
       }));
     });
 
@@ -14,6 +29,14 @@ describe('API', () => {
       await API.getFilms()
 
       expect(window.fetch).toHaveBeenCalledWith(expected);
-    })
-  })
+    });
+
+    it('should return films if the status is ok', async () => {
+      const expected = mockFilms;
+
+      const result = await API.getFilms();
+
+      expect(result).toEqual(expected);
+    });
+  });
 });
