1c72c41deefaf27517e129d3871a48d100e86d2d
Intrepid Panda <jerick999@gmail.com>
2018-10-13 20:47:47 -0600

    Start setting up mock data for response test

--- src/helper.test.js
+++ src/helper.test.js
@@ -61,16 +61,54 @@ describe('API', () => {
   });
 
   describe('getPeople', () => {
+    let url;
+    let mockResponse;
+    let mockPeople;
+
     beforeEach(() => {
-      window.fetch = jest.fn(() => {})
+      url = 'https://swapi.co/api/people/';
+
+      mockPeople = [
+      { name: 'Luke Skywalker',
+        homeworld: 'Tatooine',
+        species: 'Human',
+        homePop: '200000'},
+      { name: 'Leia Organa',
+        homeworld: 'Alderaan',
+        species: 'Human',
+        homePop: '2000000000'},
+      { name: 'R2-D2',
+        homeworld: 'Naboo',
+        species: 'Droid',
+        homePop: '4500000000'}];
+
+      mockResponse = [
+      { name: 'Luke Skywalker',
+        homeworld: 'https://swapi.co/api/planets/1/',
+        species: 'https://swapi.co/api/species/1/'},
+      { name: 'Leia Organa',
+        homeworld: 'https://swapi.co/api/planets/2/',
+        species: 'https://swapi.co/api/species/1/'},
+      { name: 'R2-D2',
+        homeworld: 'https://swapi.co/api/planets/8/',
+        species: 'https://swapi.co/api/species/2/'}];
+
+      window.fetch = jest.fn(() => Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      }));
     });
 
     it('should call fetch with the correct url', async () => {
-      const expected = 'https://swapi.co/api/people/';
-
       await API.getPeople();
 
-      expect(window.fetch).toHaveBeenCalledWith(expected);
+      expect(window.fetch).toHaveBeenCalledWith(url);
+    });
+
+    it('should return an array of people if the status is ok', async () => {  
+      const people = await API.getPeople();
+
+      expect(people).toEqual(mockPeople);
     });
   });
 });
