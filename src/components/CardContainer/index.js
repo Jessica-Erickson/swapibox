9101f2abb50544b777fc1a865eb4574b8d089cba
Intrepid Panda <jerick999@gmail.com>
2018-10-14 09:24:20 -0600

    Give CardContainer a default message

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './CardContainer.css';
 import PropTypes from 'prop-types';
 
-const CardContainer = (props) => {
+const CardContainer = ({ cardContents }) => {
+  let cards;
+
+  if ( cardContents[0] === '') {
+    cards = <h1 className="default">Select a Category!</h1>;
+  }
+
   return (
     <main className="CardContainer">
+      {
+        cards
+      }
     </main>
   );
 }
