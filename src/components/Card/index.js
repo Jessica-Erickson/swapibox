37a5740edfac50b7f74685d610467cf9d6d2c715
Intrepid Panda <jerick999@gmail.com>
2018-10-14 09:33:44 -0600

    Bring in active card logic

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,36 @@
-import React, { Component } from 'react'
-import './Card.css'
+import React, { Component } from 'react';
+import './Card.css';
+const src1 = require('./../../assets/icons/lightsaber-wt.png');
+const src2 = require('./../../assets/icons/lightsaber-bk.png');
 
 class Card extends Component {
+  constructor () {
+    super();
+    this.state = {
+      isActive: false,
+      src: src1
+    }
+  }
+
+  handleSaberClick = () => {
+    this.setState({ isActive: !this.state.isActive });
+  }
+  
   render() {
     return (
-      <article className="Card">
+      <article className={'Card ' + (this.state.isActive ? 'isAlsoLit' : 'isAlsoOff')}>
+        <h3 className={'name ' + (this.state.isActive ? 'isBlack' : 'isWhite')}>Luke Skywalker</h3>
+        <div className={'saber ' + (this.state.isActive ? 'isLit' : 'isOff')} ></div>
+        <img
+          className='hilt'
+          src={this.state.src}
+          onMouseOver={() => {this.setState({ src: src2 })}}
+          onMouseOut={() => {this.setState({ src: src1 })}}
+          alt='The hilt of a lightsaber. Turn the lightsaber on to favorite this card.'
+          onClick={() => {
+            this.setState({ src: src1 });
+            this.handleSaberClick();
+          }} />
       </article>
     )
   }
--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './CardContainer.css';
 import PropTypes from 'prop-types';
+import Card from './../Card';
 
 const CardContainer = ({ cardContents }) => {
   let cards;
 
   if ( cardContents[0] === '') {
     cards = <h1 className="default">Select a Category!</h1>;
+  } else {
+    cards = cardContents.map(thingy => {
+      return <Card contents={thingy} />
+    })
   }
 
   return (
