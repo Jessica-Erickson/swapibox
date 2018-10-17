6890b2acea20508451962cf798ac74316fc369e7
Cole Hart <cole@colehart.com>
2018-10-17 10:18:54 -0600

    Add favorites Card css for glow and refactor multiple tests

--- src/components/CardContainer/CardContainer.test.js
+++ src/components/CardContainer/CardContainer.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow , mount } from 'enzyme'
 import CardContainer from './index'
+import Card from '../Card'
 
 describe('CardContainer', () => {
   let wrapper;
@@ -17,17 +18,19 @@ describe('CardContainer', () => {
                         cardContents={cardContents}
                         currentDisplay={currentDisplay}
                         addFavorite={jest.fn()}
+                        removeFavorite={jest.fn()}
                       />);
 
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should display the Favorites default message if no Favorites', () => {
+  it('should display the favorites default message if no favorites', () => {
     currentDisplay = 'favorites'
     wrapper = shallow(<CardContainer
                     cardContents={cardContents}
                     currentDisplay={currentDisplay}
                     addFavorite={jest.fn()}
+                    removeFavorite={jest.fn()}
                   />);
 
     expect(wrapper).toMatchSnapshot();
@@ -42,8 +45,29 @@ describe('CardContainer', () => {
                       cardContents={cardContents}
                       currentDisplay={currentDisplay}
                       addFavorite={jest.fn()}
+                      removeFavorite={jest.fn()}
                     />);
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should include individual card info in addFavorite function', () => {
+    cardContents = [{ name: 'Luke Skywalker' },
+                    { name: 'Leia Organa'},
+                    { name: 'R2-D2'}];
+
+    wrapper = mount(<CardContainer
+                      cardContents={cardContents}
+                      currentDisplay={currentDisplay}
+                      addFavorite={jest.fn()}
+                      removeFavorite={jest.fn()}
+                    />);
+
+    const spy = spyOn(wrapper.find(Card).instance(), 'addFavorite')
+
+    wrapper.find(Card).find('img').simulate('click')
+    wrapper.instance().forceUpdate()
+
+    expect(spy).toHaveBeenCalled()
+  })
 });
