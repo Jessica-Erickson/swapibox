ada8bc39ae3f0c9d880106430f2db7a14fecb1cd
Intrepid Panda <jerick999@gmail.com>
2018-10-19 07:18:53 -0600

    Set up new card container structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import * as API from '../../helper.js';
 import ScrollingText from '../ScrollingText';
+import Header from '../Header';
 import './App.css';
 
 class App extends Component {
@@ -31,19 +32,41 @@ class App extends Component {
     const { filmsLoading, peopleLoading, planetsLoading, vehiclesLoading, favorites } = this.state;
     const ready = !filmsLoading && !peopleLoading && !planetsLoading && !vehiclesLoading;
 
-    return
-        <div className={ ready ? 'App' : 'loading' }>
-          <ScrollingText
-            display={ready}
-            loadingCheck={() => {
-              loadingCheck('filmsLoading');
-            }} />
-          <Header 
-            display={ready}
-            favorites={favorites.length} />
-        </div>
-      )
-    }
+    return (
+      <div className={ ready ? 'App' : 'loading' }>
+        <ScrollingText
+          display={ready}
+          loadingCheck={() => {
+            loadingCheck('filmsLoading');
+          }} />
+        <Header 
+          display={ready}
+          favorites={favorites.length} />
+        <Route exact path='/' render={() => {
+          <h2 className="default">Select a Category or Favorites</h2>
+        }}>
+        <Route exact path='/favorites' render={() => {
+          <CardContainer 
+            favorites={favorites}
+            cardContents='favorites' />
+        }} />
+        <Route exact path='/people' render={() => {
+          <CardContainer 
+            favorites={favorites}
+            cardContents='people' />
+        }} />
+        <Route exact path='/planets' render={() => {
+          <CardContainer 
+            favorites={favorites}
+            cardContents='planets' />
+        }} />
+        <Route exact path='/vehicles' render={() => {
+          <CardContainer 
+            favorites={favorites}
+            cardContents='vehicles' />
+        }} />
+      </div>
+    )
   }
 }
 
