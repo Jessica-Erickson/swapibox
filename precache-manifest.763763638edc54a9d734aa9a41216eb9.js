a0507221fd481e62bc8d9f9501bd44eb5f7abc8e
Cole Hart <cole@colehart.com>
2018-11-14 10:26:36 -0700

    Updates

---
+++ precache-manifest.763763638edc54a9d734aa9a41216eb9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/swapibox/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "5b31b3e4dd6ea6a9d6c3",
+    "url": "/swapibox/static/css/main.0a16e3a6.chunk.css"
+  },
+  {
+    "revision": "f429485c285211777e63",
+    "url": "/swapibox/static/js/1.f429485c.chunk.js"
+  },
+  {
+    "revision": "c9f58f50f745c5d4961b",
+    "url": "/swapibox/static/js/runtime~main.c9f58f50.js"
+  },
+  {
+    "revision": "0c79c07e435b8d15256b023df248a57a",
+    "url": "/swapibox/static/media/space.0c79c07e.jpg"
+  },
+  {
+    "revision": "5b31b3e4dd6ea6a9d6c3",
+    "url": "/swapibox/static/js/main.5b31b3e4.chunk.js"
+  },
+  {
+    "revision": "9445723ce85ec2b64a5acf2f368a0b16",
+    "url": "/swapibox/static/media/landspeeder.9445723c.gif"
+  },
+  {
+    "revision": "c9b2812d066f8a62ddd0a9f9341eba4d",
+    "url": "/swapibox/static/media/skywalker-fett.c9b2812d.gif"
+  },
+  {
+    "revision": "260ea6fb05f38f8ee1666ef3d9771657",
+    "url": "/swapibox/static/media/hoth.260ea6fb.gif"
+  },
+  {
+    "revision": "c8a19f313cf7fcd795bcc67b9ecf1367",
+    "url": "/swapibox/static/media/bb8-loading.c8a19f31.gif"
+  },
+  {
+    "revision": "c5d63925f63ce5de2bc57c13faf72460",
+    "url": "/swapibox/index.html"
+  }
+];
